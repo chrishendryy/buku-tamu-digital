3f484dca87d50cd4cfe5e3bdd0172e69d360c671
chrishendryy <acongturek05@gmail.com>
2025-12-24 04:30:27 +0700

    Delete genhash.mjs

--- genhash.mjs
+++
@@ -1,7 +0,0 @@
-// genhash.mjs
-import bcrypt from "bcryptjs";
-const password = "PasswordBaru123"; // password baru
-const saltRounds = 10;
-
-const hash = await bcrypt.hash(password, saltRounds);
-console.log("HASH:", hash);
